d458a65b732e38fc0a81458bc1006b6a791a414a
refael beker <refael06beker@gmail.com>
2019-03-29 00:19:38 +0200

    working

--- app/src/main/java/com/android/turtleschool/MainActivity.java
+++ app/src/main/java/com/android/turtleschool/MainActivity.java
@@ -1,12 +1,8 @@
 package com.android.turtleschool;
 
 import android.os.Bundle;
-<<<<<<< HEAD
 import android.view.View;
-=======
->>>>>>> 6d7e9cb61b447ba94348440a5864eba5eeeea777
 
-import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 import java.util.ArrayList;
@@ -15,34 +11,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-
 public class MainActivity extends AppCompatActivity {
 
-<<<<<<< HEAD
     private ArrayList<String> originalList;
-=======
->>>>>>> 6d7e9cb61b447ba94348440a5864eba5eeeea777
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         ButterKnife.bind(this);
-
-<<<<<<< HEAD
         originalList = new ArrayList<>();
         originalList.add("Alon");
         originalList.add("Alon1");
         originalList.add("Allp");
         originalList.add("Alon2");
-
     }
 
     @OnClick({R.id.tvSemester, R.id.tvStudentName, R.id.tvSubject})
     public void buttonClick(View view) {
         BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(this);
-        BottomSheetView bottomSheetView = new BottomSheetView(this,originalList);
+        BottomSheetView bottomSheetView = new BottomSheetView(this, originalList);
         bottomSheetView.setCloseListener(bottomSheetDialog::dismiss);
         bottomSheetDialog.setContentView(bottomSheetView);
         bottomSheetDialog.show();
@@ -56,17 +44,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public interface CloseListener{
+    public interface CloseListener {
         void onCloseClicked();
-=======
-    }
-
-    @OnClick(R.id.btnSheet)
-    public void btnSheet() {
-        BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(this);
-        BottomSheetView bottomSheetView = new BottomSheetView(this);
-        bottomSheetDialog.setContentView(bottomSheetView);
-        bottomSheetDialog.show();
->>>>>>> 6d7e9cb61b447ba94348440a5864eba5eeeea777
     }
 }
