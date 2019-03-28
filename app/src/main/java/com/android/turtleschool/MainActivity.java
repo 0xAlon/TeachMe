33cfad9d9b385a83994cd4e1cece81fce7db5b5c
Alon Teplitsky <alon.ttp@gmail.com>
2019-03-28 20:57:56 +0200

    Replace your computer

--- app/src/main/java/com/android/turtleschool/MainActivity.java
+++ app/src/main/java/com/android/turtleschool/MainActivity.java
@@ -2,13 +2,32 @@ package com.android.turtleschool;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+
 import androidx.appcompat.app.AppCompatActivity;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+    }
+
+    @OnClick(R.id.btnSheet)
+    public void btnSheet(){
+        BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(this);
+        bottomSheetDialog.setContentView(new BottomSheetView(this));
+        BottomSheetBehavior bottomSheetBehavior = bottomSheetDialog.getBehavior();
+        bottomSheetBehavior.setPeekHeight(2000);
+        bottomSheetDialog.show();
     }
 }
