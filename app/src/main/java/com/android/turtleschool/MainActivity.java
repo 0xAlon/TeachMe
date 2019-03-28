8a9ab064b1585950533402580d65be727d2cb36b
Alon Teplitsky <alon.ttp@gmail.com>
2019-03-28 23:02:54 +0200

    Student screen design

--- app/src/main/java/com/android/turtleschool/MainActivity.java
+++ app/src/main/java/com/android/turtleschool/MainActivity.java
@@ -1,6 +1,8 @@
 package com.android.turtleschool;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 
@@ -17,11 +19,23 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
     }
 
-    @OnClick(R.id.btnSheet)
-    public void btnSheet() {
-        BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(this);
+    @OnClick({R.id.tvSemester,R.id.tvStudentName,R.id.tvSubject})
+    public void buttonClick(View view) {
+
+        switch (view.getId()){
+            case R.id.tvSemester:
+                Toast.makeText(this, "Semester", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.tvStudentName:
+                Toast.makeText(this, "StudentName", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.tvSubject:
+                Toast.makeText(this, "Subject", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        /*BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(this);
         BottomSheetView bottomSheetView = new BottomSheetView(this);
         bottomSheetDialog.setContentView(bottomSheetView);
-        bottomSheetDialog.show();
+        bottomSheetDialog.show();*/
     }
 }
