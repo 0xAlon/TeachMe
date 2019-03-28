b6a94664d1e51a31e75b1d188a4ef5b57feb9610
AssafZfani1 <assafzfani@gmail.com>
2019-03-28 21:54:05 +0200

    working

--- app/src/main/java/com/android/turtleschool/MainActivity.java
+++ app/src/main/java/com/android/turtleschool/MainActivity.java
@@ -10,17 +10,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         ButterKnife.bind(this);
-
     }
 
     @OnClick(R.id.btnSheet)
